b31aed53d1a01b0ef67e1432a6631509e0a3c13f
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-15 18:30:43 +0100

    completed exercise

--- javascript/script.js
+++ javascript/script.js
@@ -11,33 +11,33 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49 */
 
-const grid = document.querySelector("#grid");
-
-const rows = 10;
-const columns = 10;
-const numberCells = rows * columns;
-
-
-for (let i = 0; i < numberCells; i++) {
-
-    let cells = document.createElement("div");
-
-    cells.classList.add("cell");
-
-    grid.appendChild(cells);
-
-    const randomNumberRequest = randomNumber(1, 100);
-
-
-
-    cells.innerText = randomNumberRequest;
-
-
-
+//FUNZIONI
+function createCells() {
+    let cellsToCreate = document.createElement("div");
+    cellsToCreate.classList.add("cell");
+    return cellsToCreate;
 }
-
-function randomNumber(min, max) {
-    range = max - min + 1;
-    let random = Math.floor(Math.random() * range) + min;
-    return random;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Vado a selezionare e salvare in una costante il mio elemento html della griglia
+const grid = document.querySelector("#grid");
+// definisco il numero di celle per riga
+const rows = 10;
+// definisco il numero di celle per colonna
+const columns = 10;
+// vado a eseguire una moltiplicazione per sapere il numero di celle da inserire nella griglia
+const numberCells = rows * columns;
+// creao un ciclo for che si ripeterà tante volte quante sono le celle 
+for (let i = 0; i < numberCells; i++) {
+    // richiamo la funzione che mi va a creare i div all'interno della griglia
+    let cells = createCells();
+    // vado a inserire i div all'interno della mia griglia
+    grid.appendChild(cells);
+    // inserisco numeri all'interno delle celle in maniera sequenziale
+    cells.innerText = i + 1;
+    // applico un EventListener al click che fa cambiare il colore alle celle
+    cells.addEventListener("click",
+        function () {
+            cells.classList.toggle("bgc-sky");
+        }
+    )
 }
