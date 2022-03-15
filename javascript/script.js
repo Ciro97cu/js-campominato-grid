6faeb360a5883c46a62c077875d3effe42fe65ef
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-15 15:45:48 +0100

    created grid + number inside

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,3 @@
-let prova = "Everything is Working";
-console.log(prova)
-
 /* Consegna
 creare una griglia di gioco quadrata, 
 in cui ogni cella contiene un numero tra quelli compresi 
@@ -13,3 +10,34 @@ in base al livello scelto la griglia conterrà un range diverso:
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49 */
+
+const grid = document.querySelector("#grid");
+
+const rows = 10;
+const columns = 10;
+const numberCells = rows * columns;
+
+
+for (let i = 0; i < numberCells; i++) {
+
+    let cells = document.createElement("div");
+
+    cells.classList.add("cell");
+
+    grid.appendChild(cells);
+
+    const randomNumberRequest = randomNumber(1, 100);
+
+
+
+    cells.innerText = randomNumberRequest;
+
+
+
+}
+
+function randomNumber(min, max) {
+    range = max - min + 1;
+    let random = Math.floor(Math.random() * range) + min;
+    return random;
+}
